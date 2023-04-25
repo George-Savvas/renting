854d1a983eb8d6c73faff78826326ee9ffc2defe
George-Savvas Dimitriou <sdi1800045@di.uoa.gr>
2023-04-26 01:39:08 +0300

    Main components formation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
+import Header from './Components/Header.js'
+import Home from './Components/Home.js'
+import About from './Components/About.js'
+import PageNotFound from './Components/PageNotFound.js'
+import Footer from './Components/Footer.js'
 
 export default function App() {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/*' element={<PageNotFound/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
     );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,6 @@
+export default function Footer()
+{
+    return (
+        <p>This is the footer</p>
+    )
+}
