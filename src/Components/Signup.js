a7523adbda43f008fee80a54a6db9f1aa12af37e
George-Savvas Dimitriou <sdi1800045@di.uoa.gr>
2023-05-05 19:06:03 +0300

    Signup fix

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -7,10 +7,17 @@ import './Signup.css'
 export default function Signup()
 {
     const [formData, setFormData] = React.useState({
+        id: 0,
         username: "",
         password: "",
-        passconf: ""
+        name: "",
+        lastname: "",
+        email: "",
+        telephone: "",
+        role: ""
     })
+
+    const [passconf, setPassconf] = React.useState("")
 
     function updateFormData(event)
     {
@@ -19,9 +26,22 @@ export default function Signup()
         })
     }
 
+    function updatePassConf(event)
+    {
+        setPassconf(event.target.value)
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log(`Submitted Data: ${formData.username}, ${formData.password}, ${formData.passconf}`)
+
+        console.log(JSON.stringify({ formData }))
+        console.log(`Password: ${formData.password}, Passconf: ${passconf}`)
+
+        fetch("http://localhost:7000/auth/addUser", {
+            method: 'POST',
+            body: JSON.stringify({ formData }),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        })
     }
 
     return (
@@ -30,6 +50,16 @@ export default function Signup()
                 className="signup-form"
                 onSubmit={handleSubmit}
             >
+                <label htmlFor="id" className="signup-form-label">ID</label>
+                <input
+                    className="signup-form-input"
+                    type="text"
+                    placeholder="ID"
+                    name="id"
+                    id="id"
+                    value={formData.id}
+                    onChange={updateFormData}
+                />
                 <label htmlFor="username" className="signup-form-label">Username</label>
                 <input
                     className="signup-form-input"
@@ -57,7 +87,57 @@ export default function Signup()
                     placeholder="Password Confirm"
                     name="passconf"
                     id="passconf"
-                    value={formData.passconf}
+                    value={passconf}
+                    onChange={updatePassConf}
+                />
+                <label htmlFor="name" className="signup-form-label">First Name</label>
+                <input
+                    className="signup-form-input"
+                    type="text"
+                    placeholder="First Name"
+                    name="name"
+                    id="name"
+                    value={formData.name}
+                    onChange={updateFormData}
+                />
+                <label htmlFor="lastname" className="signup-form-label">Last Name</label>
+                <input
+                    className="signup-form-input"
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastname"
+                    id="lastname"
+                    value={formData.lastname}
+                    onChange={updateFormData}
+                />
+                <label htmlFor="email" className="signup-form-label">Email</label>
+                <input
+                    className="signup-form-input"
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    id="email"
+                    value={formData.email}
+                    onChange={updateFormData}
+                />
+                <label htmlFor="telephone" className="signup-form-label">Telephone</label>
+                <input
+                    className="signup-form-input"
+                    type="text"
+                    placeholder="Telephone"
+                    name="telephone"
+                    id="telephone"
+                    value={formData.telephone}
+                    onChange={updateFormData}
+                />
+                <label htmlFor="role" className="signup-form-label">Role</label>
+                <input
+                    className="signup-form-input"
+                    type="text"
+                    placeholder="Role"
+                    name="role"
+                    id="role"
+                    value={formData.role}
                     onChange={updateFormData}
                 />
                 <input
